482c175c4406a155d810433b57fb9b00ac71f20d
Nichole John Martinez <xinmartinez@gmail.com>
2016-09-27 17:03:22 +0800

    Add util log

--- connect.js
+++ connect.js
@@ -12,18 +12,19 @@ var server = require('https').createServer(ssl.options, app);
 var io     = require("socket.io").listen(server, {"origins": ssl.origins, "pingTimeout": 30000, "pingInternal": 5000});
 var socket = io.sockets;
 var peer   = require("peer").ExpressPeerServer;
+var util   = require('./util.js');
 
 /* set app engine peer */
 app.use('/peerjs', peer(server, {debug: true}));
 
 app.use('/testing', function(req, res) {
 	res.send('Hello World!');
-	console.log("testing lang daw");
+	util.log("testing lang daw");
 });
 
 /* listen to default port 0.0.0.0 */
 var server = server.listen(ssl.port, "0.0.0.0", function(){ 
-	console.log("server listining port");
+	util.log("[NODE_SERVER] listining port", 'blue');
 });
 
 /* export */
