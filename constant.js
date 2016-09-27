653d5cf5cb26510247e4d6a8826101c2cd69b643
Nichole John Martinez <xinmartinez@gmail.com>
2016-09-27 17:03:38 +0800

    Add constants

---
+++ constant.js
@@ -0,0 +1,27 @@
+"use strict";
+/* This will be use for constants logs and settings */
+
+module.exports = {
+	/* average lesson time is 26 minutes */
+	lesson: {
+		lessonTime: 1560
+	},
+	
+	/* disconnect patterns */
+	disconnect: {
+		timewait: 30000, // 30sec timeout delay
+		teacher: {
+			finished: "teacherLessonFinished",
+			sudden: "teacherSuddenDisconnect",
+			others: "teacherLessonDisconnectOthers",
+			timeOut: "teacherTimedOut",
+			forceReconnect: "teacherForceReconnect",
+		},
+		student: {
+			finished: "studentLessonFinished",
+			sudden: "studentSuddenDisconnect",
+			timeOut: "studentTimedOut",
+			forceReconnect: "studentForceReconnect",
+		}
+	}
+};
